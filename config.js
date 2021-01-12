f942d1176d2072c92025e67d2a7b76f15481fee2
sjmiller85 <miller.scott.j@gmail.com>
2021-01-11 23:48:45 -0800

    config quality changes
    Updated the config file to include parameters for the new v1.7 quality changes.

--- config.js
+++ config.js
@@ -1,10 +1,23 @@
 module.exports = {
-  // Require submitted levels to have a score of >= 0
-  reqPosScore: true,
+  /* 
+    Require submitted levels to match at a bare minimum one of the following qualities and all subsequent qualities
+    (e.g. "AVERAGE" == AVERAGE, GOOD, and AMAZING levels):
+      "AWFUL"   -- the worst of the worst
+      "BAD"     -- not really enjoyable
+      "AVERAGE" -- the big catch all
+      "GOOD"    -- well liked levels
+      "AMAZING" -- best of the best
+      false     -- no quality check made
+  */
+  minQuality: 'AVERAGE',
+
+  // If `minQuality` is set to anything other than false,
+  // can levels with no quality (ie no upvotes and downvotes, yet) be submitted
+  naQuality: true,
 
   // Require submitters to be twitch subscribers
   subsOnly: false,
 
-  // Whether the queue is open or not
+  // Whether the queue is open or not on application startup
   queueOpen: false,
 };
